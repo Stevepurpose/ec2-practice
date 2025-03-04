5660457dcc8428848f238b2b8aa1f4c3f2feb5d0
stevepurpose <stevenpurpose@gmail.com>
2025-03-04 10:16:42 +0100

    added github workflows and minor changes to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World from Node.js on EC2!");
+    res.send("Hello World from Node.js on EC2!. This is StevePurpose");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
