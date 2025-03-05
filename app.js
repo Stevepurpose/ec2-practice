f72e2914db654cf162fc0b2d077c19685f269b18
stevepurpose <stevenpurpose@gmail.com>
2025-03-05 03:35:22 +0100

    added more output to send

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World from Node.js on EC2!. This is StevePurpose");
+    res.send("Hello World from Node.js on EC2!. This is StevePurpose, fasten your seat-belts we about to take off");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
